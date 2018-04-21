a59ad49fc35612369ae479bbd5b0ec61b40205b7
Beaglefoot <ch-e-st@yandex.ru>
2018-04-21 15:34:24 +0300

    Respond with ip, language and os

--- index.js
+++ index.js
@@ -11,14 +11,28 @@ const currentTime = getCurrentTime();
 const app = express();
 app.use(express.static('public'));
 
-app.get('*', (req, res) => {
+app.get('/api/whoami', (req, res) => {
+  // console.log('\x1b[33m', '--- GET ---', '\x1b[0m');
+
   res.set({
-    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Origin': '*'
   });
 
-  console.log('--- GET ---');
+  const { ip, headers } = req;
+  const cleanedIp = ip.replace(/^.*?((?:\d{1,3}\.){3}\d{1,3}).*$/, '$1');
+  const lang = (headers['accept-language'] || 'unknown').split(',')[0];
+  const os = (
+    [].concat(headers['user-agent'].match(/\(.+?\)/g))[0] || '(unknown)'
+  ).slice(1, -1);
+
+  res.json({ ip: cleanedIp, lang, os });
+});
+
+app.get('*', (req, res) => {
+  console.log('\x1b[33m', '--- GET ---', '\x1b[0m');
+  res.sendStatus(404);
 });
 
 app.listen(PORT, () =>
-  console.log(`[${currentTime}] express is running at ${currentIp}:${PORT}`),
+  console.log(`[${currentTime}] express is running at ${currentIp}:${PORT}`)
 );
